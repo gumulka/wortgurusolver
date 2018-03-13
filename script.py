631cb78374e8e711fa0f1cff0a8a2a5bac0dcafe
Fabian Pflug <fabian.pflug@gummu.de>
2018-03-13 19:45:50 +0100

    better command line parsing and more features

--- script.py
+++ script.py
@@ -4,15 +4,29 @@ import sys
 
 f = open("german.dic", "r", encoding="latin-1")
 
-letters = [x.upper() for x in sys.argv]
-letters.pop(0)
+args = sys.argv
+
+args.pop(0)
+
+letters = []
+number = 2
+for x in sys.argv:
+    try:
+        number = int(x)
+    except ValueError:
+        if len(x) == 1:
+            letters.append(x.upper())
+        else:
+            for l in list(x):
+                letters.append(l.upper())
 
 letterword = "".join(letters)
+
 
 for line in f:
     try:
         string = line.strip().upper()
-        if len(string) <= len(letters) and len(string) > 2:
+        if len(string) <= len(letters) and len(string) >= number:
             possible = True
             for l in list(string):
                 if l not in letters:
