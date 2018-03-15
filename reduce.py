29ddbff7e94638b3da5faf4fdccc41e100a54f81
Fabian Pflug <pflug@chi.uni-hannover.de>
2018-03-15 18:25:04 +0100

    Added check for the dictionary file

--- reduce.py
+++ reduce.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 
+import os.path
+
 
 def reduce():
+    if not os.path.isfile("german.dic"):
+        print("Please download the german dictionary \"german.dic\" from")
+        print("https://sourceforge.net/projects/germandict/" +
+              "files/german.7z/download")
+        print("and place it inside this directory.")
+        exit()
     r = open("german.dic", "r", encoding="latin-1")
     w = open("german.small", "w")
 
